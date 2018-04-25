db4061346de6a0e5d496e92f6e7d18befc9b5ed9
Justin Morris <justinjodymorris@gmail.com>
2018-04-25 10:04:32 -0700

    created Book data model in book.js, which includes an IFFE and is attached app

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,3 +1,38 @@
-'use stricts';
+'use strict';
 
-console.log('book js works');
+var app = app || {};
+
+(function(module) {
+  
+  function Book(rawDataObj) {
+    Object.key(rawDataObj).forEach( key => this[key] = rawDataObj[key]);
+  }
+  
+  Book.all = [];
+  
+  Book.prototype.toHtml() = function() {
+    var template = Handlebars.compile($('#book-list-template').text());
+  
+    // no marked .body 
+  
+    return template(this);
+  }
+  
+  Book.loadAll = bookData => {
+    Book.all = bookData.map(book => {
+      return new Book(book);
+    })
+  };
+  
+  Book.fetchAll = callback => {
+    $.get('/api/v1/books')
+    .then(results => {
+      Book.loadAll(results);
+      callback();
+    })
+    .catch(errorCallback);
+  };
+
+  module.Book = Book;
+})(app);
+
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,3 +1,2 @@
 'use strict';
 
-console.log('bookview js works');
