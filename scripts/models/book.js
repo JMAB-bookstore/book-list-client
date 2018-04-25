7d204577cc08be568af5b7deab88bec39006e8e7
Justin Morris <justinjodymorris@gmail.com>
2018-04-25 10:48:24 -0700

    created function and view containers for our book list

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,34 +3,34 @@
 var app = app || {};
 
 (function(module) {
-  
+
   function Book(rawDataObj) {
     Object.key(rawDataObj).forEach( key => this[key] = rawDataObj[key]);
   }
-  
+
   Book.all = [];
-  
-  Book.prototype.toHtml() = function() {
+
+  Book.prototype.toHtml = function() {
     var template = Handlebars.compile($('#book-list-template').text());
-  
-    // no marked .body 
-  
+
+    // no marked .body
+
     return template(this);
-  }
-  
+  };
+
   Book.loadAll = bookData => {
     Book.all = bookData.map(book => {
       return new Book(book);
-    })
+    });
   };
-  
+
   Book.fetchAll = callback => {
     $.get('/api/v1/books')
-    .then(results => {
-      Book.loadAll(results);
-      callback();
-    })
-    .catch(errorCallback);
+      .then(results => {
+        Book.loadAll(results);
+        callback();
+      });
+    // .catch(console.log('THIS IS AN ERROR'));
   };
 
   module.Book = Book;
--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,2 +1,20 @@
 'use strict';
 
+var app = app || {};
+
+(function(module) {
+
+  var bookView = {};
+
+  bookView.initIndexPage = () => {
+    $('.container').hide();
+    $('.book-view').show();
+
+    app.Book.all.forEach(e => {
+      $('#book-list').append(e.toHtml());
+    });
+  };
+
+  module.bookView = bookView;
+
+})(app) ;
